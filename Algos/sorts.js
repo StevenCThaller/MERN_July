f3ed8114a8cf113160a31d6cd23e6048d07a2419
Cody Thaller <stevencthaller@gmail.com>
2020-07-06 07:25:19 -0700

    Algos for 7/6

---
+++ Algos/sorts.js
@@ -0,0 +1,32 @@
+// Write a function to perform bubble sort.
+
+// The basic idea for bubble sort is that you want to iterate through
+// the entire array, looking at 2 elements, and swapping them if the left
+// one is greater than the right one. Basically, we're having each element
+// "bubble up" until the array is sorted.
+function bubbleSort(array) {
+
+}
+
+
+// Write a function to perform selection sort:
+// arr[] = 64 25 12 22 11
+
+// Find the minimum element in arr[0...4]
+// and place it at beginning
+// 11 25 12 22 64
+
+// Find the minimum element in arr[1...4]
+// and place it at beginning of arr[1...4]
+// 11 12 25 22 64
+
+// Find the minimum element in arr[2...4]
+// and place it at beginning of arr[2...4]
+// 11 12 22 25 64
+
+// Find the minimum element in arr[3...4]
+// and place it at beginning of arr[3...4]
+// 11 12 22 25 64 
+function selectionSort(array) {
+
+}
