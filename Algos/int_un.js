c202dacea114fa4c907fff672428c9456b643f89
Cody Thaller <stevencthaller@gmail.com>
2020-07-14 08:59:11 -0700

    Algo prompt 7/14

---
+++ Algos/int_un.js
@@ -0,0 +1,11 @@
+// Write a function that takes 2 sorted arrays and finds the intersection of those arrays.
+// The intersection is just a fancy term for the elements that are in both arrays.
+
+// EXAMPLE:
+// arr1 = [1,3,4,6]
+// arr2 = [3,5,6,7]
+// return = [3,6]
+
+function intersection(arr1, arr2) {
+
+}
