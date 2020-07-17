a562f746dd43435e8611dd18150676e62639b88c
Cody Thaller <stevencthaller@gmail.com>
2020-07-17 08:55:44 -0700

    Added algo prompt 7/17

---
+++ Algos/stair_algo.js
@@ -0,0 +1,11 @@
+// CHALLENGE ALGO TIME!!!!!
+
+// There are n number of stairs. A person at the bottom of the stairs wants to 
+// reach the top, and can climb either 1, 2, or 3 stairs at a time. 
+
+// Write an algorithm that will find the number of ways the person can reach the top.
+
+// HINT: RECURSION
+function stairs(/* what goes in here? who knows!  */){
+
+}
