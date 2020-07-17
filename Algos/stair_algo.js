d9a15bdfe16b99c69f19579aea33700a6ac5d8e7
Cody Thaller <stevencthaller@gmail.com>
2020-07-17 10:39:33 -0700

    Algo solutions 7/17

--- Algos/stair_algo.js
+++ Algos/stair_algo.js
@@ -6,6 +6,32 @@
 // Write an algorithm that will find the number of ways the person can reach the top.
 
 // HINT: RECURSION
-function stairs(/* what goes in here? who knows!  */){
+function stairs(num){
+    if(num < 0) {
+        return 0;
+    }
+    if(num == 0) {
+        return 1;
+    }
+    if(num == 1 || num == 2){
+        return num;
+    }
 
+    return stairs(num - 1) + stairs(num - 2) + stairs(num - 3);
+}
+
+// Super fancy memoization solution
+function climbStairs(n, count=0, memo=[]) {
+    if(count > n) {
+        return 0;
+    }
+    if(count === n){
+        return 1;
+    }
+    if(memo[count] > 0) {
+        return memo[count];
+    }
+
+    memo[count] = climbStairs(n, count+1, memo) + climbStairs(n, count+2, memo) + climbStairs(n, count+3, memo);
+    return memo[count];
 }
