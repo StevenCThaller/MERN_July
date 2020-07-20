85a969613bfc99a3b20f33b347ed0ebb28bcb585
Cody Thaller <stevencthaller@gmail.com>
2020-07-20 08:39:51 -0700

    Added algo prompt for 7/20

---
+++ Algos/con_sum.js
@@ -0,0 +1,11 @@
+// Write a function that takes a number, and prints the different ways
+// you can represent the number as a sum of 2 or more consecutive numbers.
+
+// Example: 
+// 15 can be represented as:
+// 1+2+3+4+5
+// 4+5+6
+// 7+8
+function conSum(num){
+    
+}
