a4cd6dc2b6cd0d45f32a9424a6493fa531e600ef
Cody Thaller <stevencthaller@gmail.com>
2020-07-21 08:57:58 -0700

    Added algo prompt for 7/21

---
+++ Algos/objtosql.js
@@ -0,0 +1,16 @@
+// Write a function that, given an object, creates a string that
+// represents a query to create a new row in a SQL database.
+
+// Don't worry about the specifics of connecting to a database or anything.
+
+// Example: objToSql(object), where object is:
+// {
+//      name: "Cody",
+//      email: "sthaller@codingdojo.com",
+//      password: "ih8mondays"
+// }
+// Would return a string:
+// `INSERT INTO table (name, email, password) VALUES ("Cody", "sthaller@codingdojo.com", "ih8mondays")`
+function objToSql(object){
+    
+}
