7fce41784fa799995ea56c52ce88f2099842294d
Cody Thaller <stevencthaller@gmail.com>
2020-07-22 08:34:56 -0700

    Algo prompt for 7/22

---
+++ Algos/chair.js
@@ -0,0 +1,8 @@
+// OOP Practice!
+
+// Utilize your OOP knowledge to create a class for a chair object.
+// What is a chair made up of? What material? What are the parts? Should those parts simply be strings?
+// Or should they be other classes? Who knows? You do!
+class Chair{
+    
+}
