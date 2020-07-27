aff2358a19596da06963c27058733b22b954751c
Cody Thaller <stevencthaller@gmail.com>
2020-07-27 11:41:58 -0700

    Websocket intro

--- Algos/canbecomepal.js
+++ Algos/canbecomepal.js
@@ -1,6 +1,46 @@
 // Write a function that takes a string,
 // and returns whether or not you can make a palindrome with that string.
 
+// "cat coat" would return true because it can be rearranged to taco cat
+// "go hang a lasagna im a salami hog" would return true because it can be rearranged to "go hang a salami im a lasagna hog"
+
+// "taco tony" would return false, because you canot make that a palindrome no matter what
+
 function canBecomePalindrome(string){
-    
+    let count = {};
+    let odds = 0;
+    let len = 0;
+
+    for(let i = 0; i < string.length; i++){
+        if(string[i] != " "){
+            if(!count[string[i]]){
+                count[string[i]] = 1;
+            } else if(count[string[i]]) {
+                count[string[i]]++;
+            }
+            len++;
+        }
+    }
+
+    for(let kv in count){
+        if(count[kv]%2 != 0){
+            odds++;
+        }
+
+        // if(odds > 0 && len%2 == 0){
+        //     return false;
+        // }
+        // else if(odds > 1 && len%2 == 1){
+        //     return false;
+        // }
+    }
+
+    if(odds > 1){
+        return false;
+    }
+
+    return true;
 }
+
+
+console.log(canBecomePalindrome("the miami marlins are already screwing things up and had to cancel a game because there is a covid outbreak")); // abcdeedcba
---
+++ BonusThings/SocketChat/client/src/App.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import './App.css';
+
+function App() {
+  const [socket] = useState(() => io(':8000'))
+  const [connected, setConnected] = useState(false);
+
+  const [uname, setUname] = useState("");
+  const [tUname, setTUname] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [chatLog, setChatLog] = useState([]);
+
+  useEffect(() => {
+    socket.emit('connected');
+
+    socket.on('success', data => setConnected(true));
+
+    socket.on('message_from_server', data => {
+      console.log(data);
+      let [...curLog] = chatLog;
+      console.log(curLog)
+      curLog.push(data);
+
+      setChatLog(curLog);
+    });
+
+    return () => socket.disconnect(true);
+  }, [])
+
+  const unameSubmit = e => {
+    e.preventDefault();
+    setUname(tUname);
+  }
+
+  const messageSubmit = e => {
+    e.preventDefault();
+    let [...curLog] = chatLog;
+
+    console.log(curLog);
+    let data = {
+      uname: uname,
+      message: message,
+      time: Date.now()
+    }
+
+
+    curLog.push(data)
+    setChatLog(curLog);
+    socket.emit('message_from_client', data);
+  }
+
+
+  return (
+    <div className="App">
+      {
+        connected ?
+        <h1>Connection established, chat away!</h1>
+        :
+        <h1>Connecting to the server...</h1>
+      }
+      {
+        connected ?
+        <form onSubmit={ unameSubmit }>
+          <label htmlFor="tUname">Username: </label>
+          <input type="text" name="tUname" onChange={ e => setTUname(e.target.value) }/>
+          <br/>
+          <input type="submit" value="Set Username"/>
+        </form>
+        :
+        ''
+      }
+      {
+        connected && uname !== "" ?
+        <div>
+          <ul>
+            {
+              chatLog.map((data, i) => 
+                <li>
+                  <p>{data.uname} at {data.time}</p>
+                  <p>{data.message}</p>
+                </li>
+              )
+            }
+          </ul>
+          <form onSubmit={ messageSubmit }>
+            <textarea name="message" cols="30" rows="10" onChange={ e => setMessage(e.target.value) }></textarea>
+            <br/>
+            <input type="submit" value="Send Message"/>
+          </form>
+        </div>
+        :
+        ''
+      }
+    </div>
+  );
+}
+
+export default App;
