a45e67d0b3613ed56c0675884a9fd54e50cac07a
Cody Thaller <stevencthaller@gmail.com>
2020-07-28 08:57:05 -0700

    Added prompt 7/28

---
+++ Algos/kMostFreq.js
@@ -0,0 +1,7 @@
+// Given an array of unsorted integers and a number, k, find the k most frequent elements in the array.
+
+// EXAMPLE: kMostFreq([4,2,9,8,2,9,2,1,5], 2) would return [2,9] since 2 is
+// found 3 times, 9 two times, and all other elements just once.
+function kMostFreq(arr, k){
+    
+}
