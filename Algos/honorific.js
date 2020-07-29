3dd5b6c1a11bb77e352a8ce7608cb01a6de72195
Cody Thaller <stevencthaller@gmail.com>
2020-07-29 07:34:25 -0700

    Added prompt 7/29

---
+++ Algos/honorific.js
@@ -0,0 +1,15 @@
+// Write a function that takes 2 strings.
+// The first string is going to be a last and first name, formatted
+// as such: "Smith, John" comma and all.
+// The second string is an honorific, such as "His Lordship", "Knight",
+// "Her Majesty", etc.. 
+
+// The function return a string such that the honorific is attached to the name,
+// but with the first name first.
+
+// EXAMPLE: 
+// honorific("Stewart, Patrick", "Sir") would return "Sir Patrick Stewart"
+
+function honorific(name, honor){
+    
+}
--- Algos/kMostFreq.js
+++ Algos/kMostFreq.js
@@ -3,5 +3,28 @@
 // EXAMPLE: kMostFreq([4,2,9,8,2,9,2,1,5], 2) would return [2,9] since 2 is
 // found 3 times, 9 two times, and all other elements just once.
 function kMostFreq(arr, k){
-    
+    let dict = {};
+    for(let i = 0; i < arr.length; i++){
+        if(!dict[arr[i]]){
+            dict[arr[i]] = 1;
+        } else {
+            dict[arr[i]]++;
+        }
+    }
+
+    let newArr = [];
+    for(let key in dict){
+        newArr.push([[key], dict[key]]);
+    }
+
+    newArr.sort((a,b) => a[1] > b[1]);
+
+    let toReturn = [];
+    for(let i = 0; i < k; i++){
+        toReturn.push(parseInt(newArr[i][0]));
+    }
+
+    return toReturn;
 }
+
+console.log(kMostFreq([1,4,3,2,3,5,1,1,2], 2));
