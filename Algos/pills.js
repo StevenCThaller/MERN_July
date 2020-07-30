39ad63a14df6cbf4f8e4af8e98223861ff958562
Cody Thaller <stevencthaller@gmail.com>
2020-07-30 09:21:40 -0700

    Save the world

---
+++ Algos/pills.js
@@ -0,0 +1,9 @@
+/*
+You have 20 bottles of pills. 1 bottle contains pills that weight 1.01 grams each. Every bottle
+OTHER than that one bottle is filled with pills that weigh 1 gram each. The pills look identical in every way
+and there's no way to feel the difference manually. The pills that weigh 1.01 grams each contain the cure
+to COVID. You also have a scale, but it only has enough battery to weigh one time. How can you weigh these pills
+in a way to guarantee that you will find the bottle that has the right pills?
+
+If you take one of the 1g pills, y'die
+*/
